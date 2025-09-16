cf39c45e0a2f1a4d1fdb2828f6ea3eb4a4e386f4
Sacha Vandermoeten <sachadu835@gmail.com>
2025-09-16 12:00:14 +0200

    feat(params): added additional params for typescript compilation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
-import path from 'path'
+import path, { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +10,20 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "index.html"), 
+        content: resolve(__dirname, "src/content/filterFeed.ts")
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    },
+    outDir: "dist",
+    emptyOutDir: true,
   },
 })
