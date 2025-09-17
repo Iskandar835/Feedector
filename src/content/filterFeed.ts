c3c4c68325b4c0a1ba40cd8050a2a4162c1cba81
Sacha Vandermoeten <sachadu835@gmail.com>
2025-09-17 10:05:24 +0200

    feat: Target DIV spotted and test injected into it + added path condition

--- src/content/filterFeed.ts
+++ src/content/filterFeed.ts
@@ -1,2 +1,40 @@
-const site = window.location.hostname;
-alert(`Le content script est bien injecté dans ${site}`);
+// THE DIV WE NEED TO FILTER
+const selector =
+  'div.scaffold-finite-scroll__content[data-finite-scroll-hotkey-context="FEED"]';
+
+
+function waitForFeed(selector: string) {
+  const applyStyle = (el: HTMLElement) => {
+    el.style.backgroundColor = "red";
+  };
+
+  const found = document.querySelector<HTMLElement>(selector);
+  if (found) {
+    applyStyle(found);
+    return;
+  }
+
+  const observer = new MutationObserver((_, obs) => {
+    const target = document.querySelector<HTMLElement>(selector);
+    if (target) {
+      applyStyle(target);
+      obs.disconnect();
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+}
+
+function onlyWorkOnFeedPage() {
+  const url = window.location.href;
+  const targetUrl = "https://www.linkedin.com/feed/";
+
+  if (url !== targetUrl) {
+    return;
+  }
+  if (url === targetUrl) {
+    waitForFeed(selector);
+  }
+}
+
+onlyWorkOnFeedPage();
